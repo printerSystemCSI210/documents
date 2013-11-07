f7714565650f29e35f7ccaa084fd2193e6ad1d57
Michael Borowsky <maborowsky@gmail.com>
2013-11-07 14:55:53 -0500

    Improved query.py

--- query.py
+++ query.py
@@ -7,10 +7,10 @@ url = 'https://192.168.130.238/hp/device/this.LCDispatcher?nav=hp.DeviceStatus'
 # Read get the html as a string
 html = urllib.request.urlopen(url).read().decode("utf8")
 
-# Parse the string (this is a pretty poor way to parse it)
-for c in range(0, len(html)):
+# Parse the string
+for c in range( 0, len(html)-7 ):
 	# Find "Tray 2"
-	if html[c]=='T' and html[c+1]=='r' and html[c+2]=='a' and html[c+3]=='y' and html[c+5]=='2':
+	if html[c:c+6]=="Tray 2":
 		# html[c+163] will be the first letter of the status of the printer 
 		if html[c+163]=='O':	# if the first letter is an "O", the status is "OK"
 			print("Tray 2: OK")
